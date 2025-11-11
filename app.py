8cbfad1c1aef09703fe1d137b813bed08eb09cd7
Bilol03 <madiyarovbilol78@gmail.com>
2025-11-11 14:38:50 +0500

    ozgina masala yechildi

---
+++ app.py
@@ -0,0 +1,38 @@
+
+# try:
+#     a , b = 10
+#     print(a, b)
+#     console.log("nimadir")
+# except NameError:
+#     print("Xato nomdan foydalanildi")
+# except TypeError:
+#     print("Nomlashda xato")
+
+# try:
+#     a = 10
+#     b = 20
+#     c = a + b
+#     print(c)
+#     a = "hello world"
+# except NameError:
+#     print("Name error chiqdi")
+# except SyntaxError:
+#     print("Syntax error chiqdi")
+# except TypeError:
+#     print("Type error chiqdi")
+# except:
+#     print("Nomalum xato")
+
+
+# a = input("Son kirit")
+# print(type(a))
+# if type(a) != "int":
+#     raise SyntaxError("Son kiritishing kerak edi")
+
+
+# son = int(input("Son kiriting: "))
+# if son % 2 == 0:
+#     print("Ajoyib, bu juft son")
+# else:
+#     raise TypeError("Toq son kiritding")
+
